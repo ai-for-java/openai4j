faccac920f5d64e8b8770360194bc56aa47505aa
deep-learning-dynamo <deep-learning-dynamo@gmail.com>
2023-06-26 22:44:21 +0200

    added an option to specify multiple inputs in moderation api

--- src/main/java/dev/ai4j/openai4j/moderation/ModerationRequest.java
+++ src/main/java/dev/ai4j/openai4j/moderation/ModerationRequest.java
@@ -3,11 +3,14 @@ package dev.ai4j.openai4j.moderation;
 import dev.ai4j.openai4j.Experimental;
 import dev.ai4j.openai4j.Model;
 
+import java.util.List;
 import java.util.Objects;
+
+import static java.util.Collections.singletonList;
 
 public class ModerationRequest {
 
-    private final String input;
+    private final List<String> input;
     private final String model;
 
     private ModerationRequest(Builder builder) {
@@ -15,7 +18,7 @@ public class ModerationRequest {
         this.model = builder.model;
     }
 
-    public String input() {
+    public List<String> input() {
         return input;
     }
 
@@ -31,15 +34,15 @@ public class ModerationRequest {
     }
 
     private boolean equalTo(ModerationRequest another) {
-        return Objects.equals(model, another.model)
-                && Objects.equals(input, another.input);
+        return Objects.equals(input, another.input)
+                && Objects.equals(model, another.model);
     }
 
     @Override
     public int hashCode() {
         int h = 5381;
-        h += (h << 5) + Objects.hashCode(model);
         h += (h << 5) + Objects.hashCode(input);
+        h += (h << 5) + Objects.hashCode(model);
         return h;
     }
 
@@ -57,15 +60,19 @@ public class ModerationRequest {
 
     public static final class Builder {
 
-        private String input;
+        private List<String> input;
         private String model;
 
         private Builder() {
         }
 
-        public Builder input(String input) {
+        public Builder input(List<String> input) {
             this.input = input;
             return this;
+        }
+
+        public Builder input(String input) {
+            return input(singletonList(input));
         }
 
         public Builder model(String model) {
