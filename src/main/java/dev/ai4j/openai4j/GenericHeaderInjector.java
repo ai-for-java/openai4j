315072e81a604355d32f7646b44bb1f169f2f864
Eric Deandrea <edeandrea@redhat.com>
2023-12-15 16:27:50 -0500

    Reformat

--- src/main/java/dev/ai4j/openai4j/GenericHeaderInjector.java
+++ src/main/java/dev/ai4j/openai4j/GenericHeaderInjector.java
@@ -10,20 +10,20 @@ import okhttp3.Request.Builder;
 import okhttp3.Response;
 
 class GenericHeaderInjector implements Interceptor {
-	private final Map<String, String> headers = new HashMap<>();
+    private final Map<String, String> headers = new HashMap<>();
 
-	GenericHeaderInjector(Map<String, String> headers) {
-		Optional.ofNullable(headers)
-			.ifPresent(this.headers::putAll);
-	}
+    GenericHeaderInjector(Map<String, String> headers) {
+        Optional.ofNullable(headers)
+            .ifPresent(this.headers::putAll);
+    }
 
-	@Override
-	public Response intercept(Chain chain) throws IOException {
-		Builder builder = chain.request().newBuilder();
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Builder builder = chain.request().newBuilder();
 
-		// Add headers
-		this.headers.forEach(builder::addHeader);
+        // Add headers
+        this.headers.forEach(builder::addHeader);
 
-		return chain.proceed(builder.build());
-	}
+        return chain.proceed(builder.build());
+    }
 }
