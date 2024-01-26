0ed3e344cdd3c9a3f41f4c998f3220a11c82f6f9
LangChain4j <info@langchain4j.dev>
2024-01-26 11:39:39 +0100

    serialize "content":null in assistant message

--- src/main/java/dev/ai4j/openai4j/Json.java
+++ src/main/java/dev/ai4j/openai4j/Json.java
@@ -4,11 +4,13 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import static com.google.gson.FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES;
+import static dev.ai4j.openai4j.AssistantMessageTypeAdapter.ASSISTANT_MESSAGE_TYPE_ADAPTER_FACTORY;
 
 class Json {
 
     static final Gson GSON = new GsonBuilder()
             .setFieldNamingPolicy(LOWER_CASE_WITH_UNDERSCORES)
+            .registerTypeAdapterFactory(ASSISTANT_MESSAGE_TYPE_ADAPTER_FACTORY)
             .registerTypeHierarchyAdapter(Enum.class, new EnumSerializer())
             .registerTypeHierarchyAdapter(Enum.class, new EnumDeserializer())
             .setPrettyPrinting()
